8fbbb8a8f8cee9e41658e7edbc931e0a238a1152
mehmetkagitli <mehmetkagitli5@gmail.com>
2025-02-24 15:49:55 +0300

    Number Guessing Game
    This project is a simple number guessing game developed in C++. The user tries to guess the randomly selected number.

---
+++ Number Guessing Game.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    srand(time(0));
+
+    int randomNum = rand() % 100 + 1;
+    int guess;
+
+    cout << "Guess a number between 1 and 100 : ";
+
+    while (true){
+    cin >> guess;
+
+    if (guess < randomNum) {
+        cout << "Please enter a larger number.";
+    }
+    else if (guess > randomNum) {
+        cout << "Please enter a smaller number.";
+    }
+    else {
+        cout << "Congratulations! You guessed it right :)";
+        break;
+    }
+    }
+
+}
---
+++ README.md.txt
@@ -0,0 +1,9 @@
+#Nummber Guessing Game
+
+This is a number guessing game written in C++.
+The program generates a random number and expects the user to find this number.
+
+#Features
+-Generates a random number between 1 and 100.
+-Receives prediction from the user and redirects (large/small).
+-When guessed correctly, it ends the game.
